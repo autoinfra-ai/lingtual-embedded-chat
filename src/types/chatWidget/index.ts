57ff8f04470b8d9dc0bf5d4c39f24415aa2dcd0d
bhaktatejas922 <bhaktatejas922@gmail.com>
2024-07-08 01:44:21 -0700

    support admin interjection and minor changes

--- src/types/chatWidget/index.ts
+++ src/types/chatWidget/index.ts
@@ -1,11 +1,12 @@
 export type ChatMessageType = {
-    message: string;
-    isSend: boolean;
-    error?: boolean;
-    bot_message_style?: React.CSSProperties;
-    user_message_style?: React.CSSProperties;
-    error_message_style?: React.CSSProperties;
-  };
+  message: string;
+  isSend: boolean;
+  error?: boolean;
+  isAdmin?: boolean;
+  bot_message_style?: React.CSSProperties;
+  user_message_style?: React.CSSProperties;
+  error_message_style?: React.CSSProperties;
+};
   
  export type suggestion = {
     questionId: number,
